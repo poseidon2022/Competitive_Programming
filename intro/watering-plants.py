a8b92a67e79f77435ce58b1521540dd72bc1833d
poseidon2022 <114685152+poseidon2022@users.noreply.github.com>
2023-12-04 18:31:09 +0300

    Added solution for Watering Plants

---
+++ intro/watering-plants.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+
+        init = capacity
+        ans = 0
+        i = 0
+        while i<len(plants):
+            print(ans)
+            if capacity<plants[i]:
+                capacity = init
+                ans+=2*i
+                continue
+            capacity-=plants[i]
+            ans+=1
+            i+=1
+        return ans
+
+        

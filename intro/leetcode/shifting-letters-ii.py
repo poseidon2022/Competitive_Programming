e634465a14dc2d308b0331d430a696e6d381c638
poseidon2022 <114685152+poseidon2022@users.noreply.github.com>
2024-02-03 20:22:49 +0300

    Add solution for Shifting Letters II

---
+++ intro/leetcode/shifting-letters-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+
+        inter = [0 for _ in range(len(s))]
+        for sta, end, dxn in shifts:
+            if dxn==1:
+                inter[sta]+=1
+                if end+1<len(s): inter[end+1]-=1
+            else:
+                inter[sta]-=1
+                if end+1<len(s): inter[end+1]+=1
+
+        
+        pref = [0 for _ in range(len(inter))]
+        pref[0] = inter[0]
+        for i in range(1, len(pref)):
+            pref[i] = pref[i-1] + inter[i]
+        
+        ans = ''
+        print(pref)
+        for i in range(len(s)):
+            tar = (ord(s[i]) - 97 + pref[i]) % 26
+            shifted_char = chr(tar + 97)
+            ans += shifted_char
+        
+        return ans
+
+
+

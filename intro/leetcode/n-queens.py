33586e81e277d7f993f62538da8a712711b0380d
poseidon2022 <114685152+poseidon2022@users.noreply.github.com>
2024-03-01 18:24:14 +0300

    Add solution for N-Queens

---
+++ intro/leetcode/n-queens.py
@@ -0,0 +1,27 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        
+        ans = []
+        def helper(arr, r, l_diag, r_diag, visited_col):
+            if len(arr) == n:
+                ans.append(arr[:])
+                return
+            
+            for col in range(n):
+                
+                if (r - col not in r_diag) and (r + col not in l_diag) and (col not in visited_col):
+                    l_diag.add(r + col)
+                    r_diag.add(r - col)
+                    visited_col.add(col)
+                    arr.append('.' * col + 'Q' + '.' * (n - col - 1))
+                    helper(arr, r + 1, l_diag, r_diag, visited_col)
+                    l_diag.remove(r + col)
+                    r_diag.remove(r - col)
+                    visited_col.remove(col)
+                    arr.pop()
+            
+        helper([], 0, set(),set(), set())
+        return ans
+
+
+                

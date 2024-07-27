1228860208a854531dff80fa1fa837d39bc7e4c8
Kidus Melaku <114685152+poseidon2022@users.noreply.github.com>
2024-07-28 01:00:44 +0300

    Created 'Count Unreachable Pairs of Nodes in an Undirected Graph' by A2SV Hub

---
+++ 27-Jul-2024/Count Unreachable Pairs of Nodes in an Undirected Graph 185334.py
@@ -0,0 +1,37 @@
+# Problem: Count Unreachable Pairs of Nodes in an Undirected Graph - https://leetcode.com/problems/count-unreachable-pairs-of-nodes-in-an-undirected-graph/
+
+class Solution:
+    def countPairs(self, n: int, edges: List[List[int]]) -> int:
+
+        total_count = (n * (n - 1)) // 2
+        graph = defaultdict(list)
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+        
+        visited = set()
+        def helper(node):
+            queue = deque([node])
+            count = 1
+            while queue:
+                cur = queue.popleft()
+                for vertex in graph[cur]:
+                    if vertex not in visited:
+                        count += 1
+                        queue.append(vertex)
+                        visited.add(vertex)
+            return count
+        
+        for node in range(n):
+            if node not in visited:
+                visited.add(node)
+                count = helper(node)
+                total_count -= (count * (count-1)) // 2
+        
+        return total_count
+
+
+        
+        
+
+
